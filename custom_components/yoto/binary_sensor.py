3c198b7d69d70ffda381b3e535ad4f7cd3c3ece1
cdnninja <jaydenaphillips@gmail.com>
2024-04-23 10:16:35 -0600

    fix: day night mode

--- custom_components/yoto/binary_sensor.py
+++ custom_components/yoto/binary_sensor.py
@@ -43,8 +43,8 @@ SENSOR_DESCRIPTIONS: Final[tuple[YotoBinarySensorEntityDescription, ...]] = (
         # off_icon="mdi:engine-off",
     ),
     YotoBinarySensorEntityDescription(
-        key="night_mode_on",
-        name="Night Mode",
+        key="day_mode_on",
+        name="Day Mode",
         is_on=lambda player: player.night_mode_on,
         # on_icon="mdi:engine",
         # off_icon="mdi:engine-off",
