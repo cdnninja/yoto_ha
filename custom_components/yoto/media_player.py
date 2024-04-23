27465be6d50aa309d4866fa46a7fab96ebd65872
cdnninja <jaydenaphillips@gmail.com>
2024-04-23 10:50:01 -0600

    Update media_player.py

--- custom_components/yoto/media_player.py
+++ custom_components/yoto/media_player.py
@@ -44,7 +44,7 @@ class YotoMediaPlayer(MediaPlayerEntity):
     ) -> None:
         # self._id = user_id
         # self.data = data
-
+        self.player = player
         self._attr_unique_id = f"{DOMAIN}_{player.id}_media_player"
 
         self._currently_playing: dict | None = {}
@@ -66,9 +66,3 @@ class YotoMediaPlayer(MediaPlayerEntity):
             return
         self.async_write_ha_state()
 
-    async def async_added_to_hass(self) -> None:
-        """When entity is added to hass."""
-        await super().async_added_to_hass()
-        self.async_on_remove(
-            self.data.devices.async_add_listener(self._handle_devices_update)
-        )
