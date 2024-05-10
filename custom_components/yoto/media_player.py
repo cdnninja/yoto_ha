5efc12cf4455ec1a00883689ce1ca3f56d721d64
cdnninja <jaydenaphillips@gmail.com>
2024-05-10 17:39:22 -0600

    fix: expose play

--- custom_components/yoto/media_player.py
+++ custom_components/yoto/media_player.py
@@ -67,7 +67,7 @@ class YotoMediaPlayer(MediaPlayerEntity, YotoEntity):
     @property
     def supported_features(self) -> MediaPlayerEntityFeature:
         """Return the supported features."""
-        return MediaPlayerEntityFeature.PAUSE
+        return MediaPlayerEntityFeature.PAUSE | MediaPlayerEntityFeature.PLAY
 
     @property
     def state(self) -> MediaPlayerState:
