69d26cc9c4e22aecb494842683c404a379d846e6
cdnninja <jaydenaphillips@gmail.com>
2024-05-31 09:26:43 -0600

    Update number.py

--- custom_components/yoto/number.py
+++ custom_components/yoto/number.py
@@ -71,7 +71,7 @@ async def async_setup_entry(
     for player_id in coordinator.yoto_manager.players.keys():
         player: YotoPlayer = coordinator.yoto_manager.players[player_id]
         for description in SENSOR_DESCRIPTIONS:
-            if getattr(player, description.key, None) is not None:
+            if getattr(player.config, description.key, None) is not None:
                 entities.append(YotoNumber(coordinator, description, player))
     async_add_entities(entities)
     return True
