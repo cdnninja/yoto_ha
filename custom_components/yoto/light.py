cd1a59f1872d74391fbad6358793d06da7353500
cdnninja <jaydenaphillips@gmail.com>
2024-06-07 22:08:58 -0600

    next commit

--- custom_components/yoto/light.py
+++ custom_components/yoto/light.py
@@ -3,8 +3,10 @@
 from __future__ import annotations
 
 import logging
+from typing import Final
 
 from yoto_api import YotoPlayer
+from .utils import rgetattr
 
 from homeassistant.components.light import (
     LightEntity,
@@ -22,6 +24,17 @@ from .entity import YotoEntity
 
 _LOGGER = logging.getLogger(__name__)
 
+SENSOR_DESCRIPTIONS: Final[tuple[LightEntityDescription, ...]] = (
+    LightEntityDescription(
+        key="config.day_ambient_colour",
+        name="Day Ambient Colour",
+    ),
+        LightEntityDescription(
+        key="config.night_ambient_colour",
+        name="Night Ambient Colour",
+    ),
+)
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -33,12 +46,14 @@ async def async_setup_entry(
     entities = []
     for player_id in coordinator.yoto_manager.players.keys():
         player: YotoPlayer = coordinator.yoto_manager.players[player_id]
-        entities.append(YotoSensor(coordinator, player))
+        for description in SENSOR_DESCRIPTIONS:
+            if rgetattr(player, description.key) is not None:
+                entities.append(YotoLight(coordinator, description, player))
     async_add_entities(entities)
     return True
 
 
-class YotoSensor(LightEntity, YotoEntity):
+class YotoLight(LightEntity, YotoEntity):
     """Yoto sensor class."""
 
     def __init__(
@@ -57,11 +72,36 @@ class YotoSensor(LightEntity, YotoEntity):
 
     @property
     def color_mode(self):
-        """Return the color modes the sensor supports."""
-        return ColorMode.RGBW
+        """Return the color mode."""
+        return ColorMode.RGB
 
     @property
     def supported_color_modes(self):
-        """Return the unit the value was reported in by the sensor"""
+        """Return the color modes the sensor supports."""
+        return [ColorMode.RGB]
+    
+    @property
+    def rgb_color(self):
+        """Return the RGB color"""
+        hex_val = input(getattr(self.player, self._key)).lstrip('#')
+        rgb_val = tuple(int(hex_val[i:i+2], 16) for i in (0, 2, 4))
+        return rgb_val
+    
+    @property
+    def is_on(self):
+        """Return if the light is on."""
+        status = getattr(self.player, self._key)
+        if status is not "#0":
+            return True
+        else:
+            return False
+        
+    async def async_turn_off(self, **kwargs):
+        """Turn device off."""
+        await self.coordinator.async_set_light(self.player.id, self._key, "#0")
+        self.async_write_ha_state()
 
-        return self._description.native_unit_of_measurement
+    async def async_turn_on(self, **kwargs):
+        """Turn device off."""
+        await self.coordinator.async_set_light(self.player.id, self._key, "#ffffff")
+        self.async_write_ha_state()
