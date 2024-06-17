75192f609b818409fcf212c52b1f9a46b0b7b579
cdnninja <jaydenaphillips@gmail.com>
2024-06-16 19:06:35 -0600

    Update media_player.py

--- custom_components/yoto/media_player.py
+++ custom_components/yoto/media_player.py
@@ -22,6 +22,7 @@ from homeassistant.components.media_player import (
     MediaPlayerEnqueue,
     BrowseMedia,
     MediaType,
+    MediaClass,
 )
 
 from .const import DOMAIN
@@ -97,25 +98,18 @@ class YotoMediaPlayer(MediaPlayerEntity, YotoEntity):
         media_content_id: str | None = None,
     ) -> BrowseMedia:
         """Implement the websocket media browsing helper."""
-        self.thumbnail_cache = {}
-        if media_content_type in (None, "library"):
-            return await browse_top_level(self._volumio)
 
-        return await browse_node(
-            self, self._volumio, media_content_type, media_content_id
-        )
-
-    async def async_get_browse_image(
-        self,
-        media_content_type: MediaType | str,
-        media_content_id: str,
-        media_image_id: str | None = None,
-    ) -> tuple[bytes | None, str | None]:
-        """Get album art from Volumio."""
-        cached_url = self.thumbnail_cache.get(media_content_id)
-        image_url = self._volumio.canonic_url(cached_url)
-        return await self._async_fetch_image(image_url)
-
+        if media_content_id in (None, "library"):
+            children = await self.async_convert_library_to_browse_media()
+            return BrowseMedia(media_content_id="Root", media_class=MediaClass.DIRECTORY, media_content_type=MediaType.MUSIC, title="Yoto Library", can_expand=True, can_play=False, children=children, children_media_class=MediaClass.MUSIC)
+        
+    
+    async def async_convert_library_to_browse_media(self) -> list:
+        children = []
+        for card in self.coordinator.yoto_manager.library.values():
+            children.append(BrowseMedia(media_content_id=card.id,media_class=MediaClass.MUSIC, media_content_type=MediaType.MUSIC, title=card.title, can_expand=False, can_play=True))
+        return children
+    
     @property
     def supported_features(self) -> MediaPlayerEntityFeature:
         """Return the supported features."""
