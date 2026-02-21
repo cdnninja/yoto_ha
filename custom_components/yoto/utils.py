552bd8bcea1134128ede6a4f9dcf800faadc87a8
cdnninja <jaydenaphillips@gmail.com>
2026-02-21 12:42:18 -0700

    fix: logger error

--- custom_components/yoto/utils.py
+++ custom_components/yoto/utils.py
@@ -27,7 +27,7 @@ def split_media_id(text: str) -> tuple[str, str | None, str | None, int]:
     Format: cardid+chapterid+trackid+seconds
     """
     if text.count("-") > 1:
-        _LOGGER.ERROR("Switch Media ID format to use + as separator instead of -")
+        _LOGGER.error("Switch Media ID format to use + as separator instead of -")
     parts = text.split("+")
     if len(parts) == 4:
         cardid, chapterid, trackid, time_str = parts
